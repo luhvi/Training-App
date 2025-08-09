31340a7a8464c88c055c61dc34c965f63daa7ee9
luhvi <luhvilive@gmail.com>
2025-08-09 12:55:40 +0200

    removed some glow

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -5,9 +5,7 @@ const Button = ({ text, long }: ButtonProps) => {
     <button
       className={`cursor-pointer py-4 text-lg font-medium text-[oklch(0.95_0_0)] ring-1 ring-white/60 transition-all duration-300 ease-in-out hover:bg-indigo-700/40 hover:ring-indigo-500 ${long ? "w-full rounded-full" : "rounded-full px-8"}`}
     >
-      <span className="drop-shadow-[0_0_4px_rgba(255,255,255,0.50)]">
-        {text}
-      </span>
+      {text}
     </button>
   );
 };
--- components/ui/PricingCard.tsx
+++ components/ui/PricingCard.tsx
@@ -8,7 +8,7 @@ const PricingCard = ({ title, price, subscriptionInfo }: PricingCardProps) => {
     <div
       className={`relative h-90 w-full rounded-2xl bg-black/10 p-8 ring-1 ring-white/60 backdrop-blur-2xl transition-all duration-300 ease-in-out hover:ring-indigo-500 ${title === "premiumSubscription" ? "mb-10 lg:mr-10 lg:mb-10" : ""} `}
     >
-      <h1 className="mb-2 cursor-default text-2xl font-medium text-[oklch(0.95_0_0)]">
+      <h1 className="mb-2 cursor-default text-2xl font-medium text-[oklch(0.90_0_0)]">
         {title === "premiumSubscription" ? "Premium" : "7-Day Free Trial"}
       </h1>
       <div className="mb-6">
